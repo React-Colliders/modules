9200972c07509aac0c574c975c1850c77435f89c
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-07-10 21:51:18 +0530

    modules concept

---
+++ a.js
@@ -0,0 +1,31 @@
+
+console.log("File starting...")
+
+let name = "John" 
+
+ export function display(){
+    console.log("Hello", name)
+}
+
+
+ function hello(){
+    console.log("Hi I am dancing")
+}
+
+
+console.log("File ending...")
+
+export default hello
+
+
+
+
+// function hello(){
+//     console.log("Hi I am dancing")
+// }
+
+// h1 = hello
+
+// h1() 
+
+// hello()
---
+++ b.js
@@ -0,0 +1,19 @@
+// import "./a.js"; // loading and executing that file
+// import hello from "./a.js";
+
+// import {} from "./a.js";
+// import display from "./a.js";
+
+// import xyz from "./a.js";
+
+import  display from "./a.js"; 
+
+
+
+display()
+
+
+
+
+
+// import "./a.js" // load and execute a file
